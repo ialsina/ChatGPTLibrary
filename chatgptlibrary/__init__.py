cc9f88951886f0b00c574f0d67cbdd992b04803b
Ivan Alsina <50876898+ialsina@users.noreply.github.com>
2025-05-24 13:27:06 +0200

    Add version

--- chatgptlibrary/__init__.py
+++ chatgptlibrary/__init__.py
@@ -1,4 +1,5 @@
 from .chatlibrary import ChatLibrary
 from .chat import Chat
+from ._version import __version__
 
-__all__ = ["ChatLibrary", "Chat"]
+__all__ = ["ChatLibrary", "Chat", "__version__"]
